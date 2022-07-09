1099f837a59656a7c3310e52a1d02ea608f272b8
divyachopra14 <72263840+divyachopra14@users.noreply.github.com>
2022-07-09 11:45:44 +0530

    Add files via upload

---
+++ Automation Projects/linkedin.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+for i in range(11):
+    pyautogui.click(x=1582, y=393)
+    time.sleep(0.2)
---
+++ Automation Projects/tinder.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+for i in range(11):
+    pyautogui.click(x=1281, y=849)
+    time.sleep(0.2)
